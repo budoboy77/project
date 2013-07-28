f212962ab4614f50f60d387853be5a80f17c3746
Bryan <bew@bryanewhite.com>
2013-07-27 21:29:42 -0600

    Created migration and added some tables to migrate file.

---
+++ db/migrate/20130728032028_create_schema.rb
@@ -0,0 +1,24 @@
+class CreateSchema < ActiveRecord::Migration
+  def up
+  	create_table :questions do |t|
+	  	t.string :category
+	  	t.string :answer
+	  	t.text :explaination
+  	end
+  	create_table :categories do |t|
+  		t.string :category_name
+  	end
+
+  	create_table :users do |t|
+  		t.string :user_name
+  		t.string :email
+  		t.string :role
+  		t.string :last_questionnaire
+end
+
+  def down
+  	drop_table :questions
+  	drop_table :categories
+  	drop_table :users
+  end
+end
