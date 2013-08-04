748879da7b08845f814716e9f68a41bb2ebcc40b
Bryan <bew@bryanewhite.com>
2013-08-03 22:24:59 -0600

    Edited migration file.

--- db/migrate/20130728032028_create_schema.rb
+++ db/migrate/20130728032028_create_schema.rb
@@ -1,25 +1,18 @@
 class CreateSchema < ActiveRecord::Migration
   def up
   	create_table :questions do |t|
-	  	t.string :category
-	  	t.string :answer
-	  	t.text :explaination
-  	end
-  	create_table :categories do |t|
-  		t.string :category_name
-  	end
-
-  	create_table :users do |t|
-  		t.string :user_name
-  		t.string :email
-  		t.string :role
-  		t.string :last_questionnaire
+      t.string :topic
+      t.string :type
+      t.text :question_text
+	  	t.string :answer1
+      t.string :answer2
+      t.string :answer3
+      t.string :answer4
+	  	t.string :correct_answer
   	end
 end
 
   def down
   	drop_table :questions
-  	drop_table :categories
-  	drop_table :users
   end
 end
