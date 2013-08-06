af66ab55f6e60cec9a857b867219c8b71c2a126c
Bryan <bew@bryanewhite.com>
2013-08-06 17:44:57 -0600

    Added put handler for add question form.
    Also added changes to questions.erb file.

--- controller.rb
+++ controller.rb
@@ -4,6 +4,7 @@ require './dvc-sinatra.rb'
 
 get '/quiz' do
 	@title = "quiz"
+	
 	halt erb(:quiz)
 end
 
@@ -25,4 +26,26 @@ get '/questions' do
 	@title = "Questions"
 	@questions = Question.all
 	halt erb(:questions)
+end
+
+post '/questions' do
+	new_question = Question.new
+	new_question.question_topic = params[:topic]
+	new_question.question_type 	= params[:type]
+	new_question.question_text 	= params[:text]
+	new_question.choice1		= params[:choice1]
+	new_question.choice2		= params[:choice2]
+	new_question.choice3		= params[:choice3]
+	new_question.choice4		= params[:choice4]
+	if params[:answer1] != nil
+		new_question.correct_answer	= params[:choice1]
+	elsif params[:answer2] != nil
+		new_question.correct_answer	= params[:choice2]
+	elsif params[:answer3] != nil
+		new_question.correct_answer	= params[:choice3]
+	else params[:answer4] != nil
+		new_question.correct_answer	= params[:choice4]
+	end
+	new_question.save!
+	redirect "/questions"
 end
