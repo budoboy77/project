38bb56a945bc44fe9d5727223c2ba601dce42e54
Bryan <bew@bryanewhite.com>
2013-08-09 10:41:44 -0600

    Fixed routed to allow edit on questions page.

--- controller.rb
+++ controller.rb
@@ -24,34 +24,59 @@ get '/admin' do
 end
 
 get '/questions' do
-	if params[:id] != nil
+		@title = "Questions"
+		@questions = Question.order("id desc").all
+		halt erb(:questions)
+end
+
+get '/questions/:id' do
 		@edit_question = Question.find(params[:id])
-	else
-	end
 		@title = "Questions"
 		@questions = Question.order("id desc").all
 		halt erb(:questions)
 end
 
 post '/questions' do
-	new_question = Question.new
-	new_question.question_topic = params[:topic]
-	new_question.question_type 	= params[:type]
-	new_question.question_text 	= params[:text]
-	new_question.choice1		= params[:choice1]
-	new_question.choice2		= params[:choice2]
-	new_question.choice3		= params[:choice3]
-	new_question.choice4		= params[:choice4]
+	question = Question.new
+	question.question_topic = params[:topic]
+	question.question_type 	= params[:type]
+	question.question_text 	= params[:text]
+	question.choice1		= params[:choice1]
+	question.choice2		= params[:choice2]
+	question.choice3		= params[:choice3]
+	question.choice4		= params[:choice4]
 	if params[:answer1] != nil
-		new_question.correct_answer	= params[:choice1]
+		question.correct_answer	= params[:choice1]
 	elsif params[:answer2] != nil
-		new_question.correct_answer	= params[:choice2]
+		question.correct_answer	= params[:choice2]
 	elsif params[:answer3] != nil
-		new_question.correct_answer	= params[:choice3]
+		question.correct_answer	= params[:choice3]
 	else params[:answer4] != nil
-		new_question.correct_answer	= params[:choice4]
+		question.correct_answer	= params[:choice4]
 	end
-	new_question.save!
+	question.save!
+	redirect "/questions"
+end
+
+post '/questions/:id' do
+	question = Question.find(params[:id])
+	question.question_topic = params[:topic]
+	question.question_type 	= params[:type]
+	question.question_text 	= params[:text]
+	question.choice1		= params[:choice1]
+	question.choice2		= params[:choice2]
+	question.choice3		= params[:choice3]
+	question.choice4		= params[:choice4]
+	if params[:answer1] != nil
+		question.correct_answer	= params[:choice1]
+	elsif params[:answer2] != nil
+		question.correct_answer	= params[:choice2]
+	elsif params[:answer3] != nil
+		question.correct_answer	= params[:choice3]
+	else params[:answer4] != nil
+		question.correct_answer	= params[:choice4]
+	end
+	question.save!
 	redirect "/questions"
 end
 
